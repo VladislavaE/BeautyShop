41078e2ef37bd41de16bdf539977a43740b6f3fc
Владислава Ерохина <vyuerokhina@edu.hse.ru>
2018-06-06 20:15:56 +0300

    Добавьте файлы проекта.

---
+++ Library/Masters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Masters
+    {
+        public string Name { get; set; }
+        public List<Services> Skills { get; set; }
+        public Schedule Schedule { get; set; }
+    }
+}
